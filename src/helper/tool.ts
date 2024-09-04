9ac2d61a64a3835094b8496507ae2dfd1d068c07
zhoujia6139 <zhoujia6139@126.com>
2024-09-04 14:34:09 +0800

    parse erc20 info.

--- src/helper/tool.ts
+++ src/helper/tool.ts
@@ -45,7 +45,23 @@ export function parseWithdrawTxInfo(
   const value = '0x' + withdrawInfoData.slice(84, 116).toString('hex');
   const gasLimit = '0x' + withdrawInfoData.slice(116, 148).toString('hex');
   const data = '0x' + withdrawInfoData.slice(148).toString('hex');
-
+  const ethValue = parseInt(value, 16);
+  console.log(`ethValue: ${ethValue}`);
+  if (ethValue === 0) {
+    console.log("list erc20 cross chain info:");
+    const l1Token = '0x' + data.slice(490, 530);
+    const l2Token = '0x' + data.slice(530, 594);
+    const from = '0x' + data.slice(594, 658);
+    const to = '0x' + data.slice(682, 722);
+    const amount = '0x' + data.slice(722, 786);
+    const amountInDecimal12 = parseInt(amount, 16);
+    console.log(`l1Token: ${l1Token}`);
+    console.log(`l2Token: ${l2Token}`);
+    console.log(`from: ${from}`);
+    console.log(`to: ${to}`);
+    console.log(`amount: ${amount}`);
+    console.log(`amountInDecimal12: ${amountInDecimal12}`);
+  }
   return {
     nonce,
     sender,
